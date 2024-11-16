9a037dd89131d43a04f55f0389204729a5592f32
Leon Bruns <ujiqk@student.kit.edu>
2024-11-16 21:22:22 +0100

    simple image detection demo 2 yolo v10 n with auto camera

--- web/index.js
+++ web/index.js
@@ -25,9 +25,9 @@ camera_button.addEventListener(
         imageContainer.innerHTML = "";
         const image = document.createElement("img");
         image.src = canvas.toDataURL('image/png');
-        image.onload = () => detect(image);
         imageContainer.appendChild(image);
         //detect(image);  // Uncomment this line to run the model
+        image.onload = () => detect(image);
         ev.preventDefault();
     },
     false,
@@ -45,25 +45,56 @@ const model = await AutoModel.from_pretrained('onnx-community/yolov10n', {
 })
 const processor = await AutoProcessor.from_pretrained('onnx-community/yolov10n');
 status.textContent = "Ready";
+autoDetect();
 
 async function detect(imageElement) {
-    const url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/city-streets.jpg';
-    const image = await RawImage.read(url);
+    // Read image and run processor
+    const image = await RawImage.read(imageElement.src);
     const { pixel_values, reshaped_input_sizes } = await processor(image);
-
-// Run object detection
+    // Run object detection
     const { output0 } = await model({ images: pixel_values });
     const predictions = output0.tolist()[0];
-
-    const threshold = 0.7;
+    const threshold = 0.6;
     const [newHeight, newWidth] = reshaped_input_sizes[0]; // Reshaped height and width
     const [xs, ys] = [image.width / newWidth, image.height / newHeight]; // x and y resize scales
     for (const [xmin, ymin, xmax, ymax, score, id] of predictions) {
         if (score < threshold) continue;
-
         // Convert to original image coordinates
         const bbox = [xmin * xs, ymin * ys, xmax * xs, ymax * ys].map(x => x.toFixed(2)).join(', ');
         console.log(`Found "${model.config.id2label[id]}" at [${bbox}] with score ${score.toFixed(2)}.`);
+        result.textContent = model.config.id2label[id];
     }
 }
+
+
+async function autoDetect() {
+    const video = document.getElementById('video');
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const image = document.createElement("img");
+    image.src = canvas.toDataURL('image/png');
+    image.onload = async () => {
+        // Read image and run processor
+        const rawImage = await RawImage.read(image.src);
+        const {pixel_values, reshaped_input_sizes} = await processor(rawImage);
+        // Run object detection
+        const {output0} = await model({images: pixel_values});
+        const predictions = output0.tolist()[0];
+        const threshold = 0.6;
+        const [newHeight, newWidth] = reshaped_input_sizes[0]; // Reshaped height and width
+        const [xs, ys] = [rawImage.width / newWidth, rawImage.height / newHeight]; // x and y resize scales
+        for (const [xmin, ymin, xmax, ymax, score, id] of predictions) {
+            if (score < threshold) continue;
+            // Convert to original image coordinates
+            const bbox = [xmin * xs, ymin * ys, xmax * xs, ymax * ys].map(x => x.toFixed(2)).join(', ');
+            console.log(`Found "${model.config.id2label[id]}" at [${bbox}] with score ${score.toFixed(2)}.`);
+            result.textContent = result.textContent + "\n" + model.config.id2label[id];
+        }
+        setTimeout(autoDetect, 100);
+    }
+}
+
 
